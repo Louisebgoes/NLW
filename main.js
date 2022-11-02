dbabae5319e3378e4aa2eb15e803cf2c3b8a6731
Louise <louiserpb@hotmail.com>
2022-11-02 14:30:01 -0300

    introduzindo javascript

---
+++ main.js
@@ -0,0 +1,24 @@
+document.querySelector("#app").innerHTML = `
+<header>
+        <img src="./assets/logo1.svg" alt="logo da nlw" />
+      </header>
+
+      <main id="cards">
+        <div class="card">
+          <h2>24/11 <span> Quinta </span></h2>
+          <ul>
+            <li>
+              <img src="./assets/suicaa.svg" alt="bandeira da suica" />
+              <strong> 7:00 </strong>
+              <img src="./assets/cameroon.svg" alt="bandeira dos camaroes" />
+            </li>
+
+            <li>
+              <img src="./assets/brasil.svg" alt="bandeira do brasil" />
+              <strong> 16:00 </strong>
+              <img src="./assets/serbia.svg" alt="bandeira da servia" />
+            </li>
+          </ul>
+        </div>
+      </main>
+`
