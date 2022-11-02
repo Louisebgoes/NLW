ef9bdf06b162ce1606174c17700e0224d7115a31
Louise <louiserpb@hotmail.com>
2022-11-02 14:44:54 -0300

    javascript

--- main.js
+++ main.js
@@ -1,10 +1,6 @@
-document.querySelector("#app").innerHTML = `
-<header>
-        <img src="./assets/logo1.svg" alt="logo da nlw" />
-      </header>
-
-      <main id="cards">
-        <div class="card">
+function creatCard() {
+  return `
+  <div class="card">
           <h2>24/11 <span> Quinta </span></h2>
           <ul>
             <li>
@@ -12,12 +8,20 @@ document.querySelector("#app").innerHTML = `
               <strong> 7:00 </strong>
               <img src="./assets/cameroon.svg" alt="bandeira dos camaroes" />
             </li>
+         
+  `
+}
 
-            <li>
-              <img src="./assets/brasil.svg" alt="bandeira do brasil" />
-              <strong> 16:00 </strong>
-              <img src="./assets/serbia.svg" alt="bandeira da servia" />
-            </li>
+document.querySelector("#app").innerHTML = `
+<header>
+        <img src="./assets/logo1.svg" alt="logo da nlw" />
+      </header>
+
+      <main id="cards">
+        ${creatCard()}
+        ${creatCard()}
+        ${creatCard()}
+            
           </ul>
         </div>
       </main>
